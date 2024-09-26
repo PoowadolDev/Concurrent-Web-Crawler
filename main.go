b8bcf21e82e5e85062edf386cbd1a4bd3416dbd6
PoowadolDev <poowadol.dev@gmail.com>
2024-09-26 21:43:15 +0700

    Add Worker and VisitedURLs: goruntine, channel, wait groups and mux for manage concurrent

--- main.go
+++ main.go
@@ -2,23 +2,72 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 )
+
+type VisitedURLs struct {
+	urls map[string]bool
+	mux  sync.Mutex
+}
 
 func main() {
 	fmt.Println("Starting Concurrent Web Crawler")
+	startURL := "https://www.w3schools.com/"
+	urls := make(chan string)
+	visited := &VisitedURLs{urls: make(map[string]bool)}
+	var wg sync.WaitGroup
 
-	content, err := fetch("https://www.w3schools.com/")
-	if err != nil {
-		fmt.Println("Error fetching pages:", err)
+	numWorkers := 5
+	for i := 0; i < numWorkers; i++ {
+		wg.Add(1)
+		go worker(i, &wg, urls, visited)
 	}
 
-	links, err := parseLinks(content)
-	if err != nil {
-		fmt.Println("Error parsing pages:", err)
-	}
+	urls <- startURL
 
-	fmt.Println("Found links:")
-	for _, link := range links {
-		fmt.Println(link)
+	go func() {
+		time.Sleep(10 * time.Second)
+		close(urls)
+	}()
+
+	wg.Wait()
+}
+
+func (v *VisitedURLs) IsVisited(url string) bool {
+	v.mux.Lock()
+	defer v.mux.Unlock()
+	return v.urls[url]
+}
+
+func (v *VisitedURLs) Add(url string) {
+	v.mux.Lock()
+	defer v.mux.Unlock()
+	v.urls[url] = true
+}
+
+func worker(id int, wg *sync.WaitGroup, urls chan string, visited *VisitedURLs) {
+	defer wg.Done()
+	for url := range urls {
+		if visited.IsVisited(url) {
+			continue
+		}
+
+		visited.Add(url)
+		fmt.Printf("Worker %d fetching URL: %s\n", id, url)
+		content, err := fetch(url)
+		if err != nil {
+			fmt.Println("Error fetching page:", err)
+			continue
+		}
+
+		links, err := parseLinks(content)
+		if err != nil {
+			fmt.Println("Error parsing page:", err)
+			continue
+		}
+		for _, link := range links {
+			urls <- link
+		}
 	}
 }
